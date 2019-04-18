6a8a22848818f509b6e6af979549c3ca6a91d83c
togogo <togokida@gmail.com>
2019-04-17 22:42:25 -0400

    motor/FSR addition

--- STU1232_Cane_Prototype_Integrated.ino
+++ STU1232_Cane_Prototype_Integrated.ino
@@ -12,6 +12,12 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);//for BNO055
 #define RxD 2
 #define LED_PIN 13
 
+//Define FSR pin
+#define fsrpin A0 //The FSR and 10K pulldown resistor are connected to A0.
+
+//Define variables
+int fsrreading; //Variable to store FSR value
+
 SoftwareSerial Bluetooth(TxD, RxD);
 char c;
 
@@ -19,59 +25,74 @@ int led = 9;//digital 9 for the white LED
 int brightness = 0;
 int fadeAmount = 5;
 
+int vibrationMotor = 6;
+
 void setup() {
 
   //start comm. for both serial and bluetooth
   Bluetooth.begin(9600);
   Serial.begin(9600);
-  
+
   pinMode(LED_PIN, OUTPUT);
   pinMode(led, OUTPUT);//for led
+  pinMode(vibrationMotor, OUTPUT);
 
 
   Serial.println("Orientation Sensor Test"); Serial.println("");
-  
+
   /* Initialise the sensor */
-  if(!bno.begin())
+  if (!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
-  
-  delay(1000);
-    
-  bno.setExtCrystalUse(true);
+    while (1);
   }
 
+  delay(1000);
+
+  bno.setExtCrystalUse(true);
+}
+
 void loop() {
+
+  fsrreading = analogRead(fsrpin);  //Reads the FSR pin and stores the output as fsrreading
+  //Prints the fsrreading in the serial monitor.
+  Serial.print("Analog reading = ");  //Print the string "Analog reading = ".
+  Serial.print(fsrreading);           //Prints the fsrreading.
+  Serial.println("");
+
+  //for the vibration motor part...could be refined
+  analogWrite(vibrationMotor, 150);
+  delay(200);
+  analogWrite(vibrationMotor, 0);
 
   //for the white led...could be refined.
   analogWrite(led, brightness);
   brightness = brightness + fadeAmount;
-  if(brightness <= 0 || brightness >= 255){
+  if (brightness <= 0 || brightness >= 255) {
     fadeAmount = -fadeAmount;
   }
 
-  /*BNO055 code: Get a new sensor event */ 
-  sensors_event_t event; 
+  /*BNO055 code: Get a new sensor event */
+  sensors_event_t event;
   bno.getEvent(&event);
-  
+
   //this sends serial signal from PC -> Arduino
   //Going to use this to turn on the LED Lamp
-  if(Bluetooth.available()){
+  if (Bluetooth.available()) {
     c = Bluetooth.read();
     Serial.println(c);
-    
-    if(c=='1'){
-      digitalWrite(LED_PIN, HIGH);          
+
+    if (c == '1') {
+      digitalWrite(LED_PIN, HIGH);
     }
-    if(c=='0'){
+    if (c == '0') {
       digitalWrite(LED_PIN, LOW);
     }
   }
 
   /* Display the floating point data for SERIAL comm. */
+  /*
   Serial.print("X: ");
   Serial.print(event.orientation.x, 4);
   Serial.print("\tY: ");
@@ -79,6 +100,7 @@ void loop() {
   Serial.print("\tZ: ");
   Serial.print(event.orientation.z, 4);
   Serial.println("");
+  */
 
   /*Display the floating point data for BLUETOOTH comm. */
   Bluetooth.print("X: ");
@@ -88,6 +110,6 @@ void loop() {
   Bluetooth.print("\tZ: ");
   Bluetooth.print(event.orientation.z, 4);
   Bluetooth.println("");
-  
+
   delay(100);//need to play around with this variable
 }
